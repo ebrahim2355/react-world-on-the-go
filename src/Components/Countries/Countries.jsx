054844626d953a781543af3c23dd562a62b4572c
ebrahim2355 <web.ebrahimali@gmail.com>
2025-09-30 02:18:52 +0600

    second

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+
+    const [visitedFlags, setVisitedFlags] = useState([])
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (country) => {
+        console.log("Clicked", country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = (flag) => {
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
+
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries
+    // console.log(countries);
     return (
         <div>
-            <h1>In The Countries</h1>
+            <h1>In The Countries: {countries.length}</h1>
+            <h3>Total Country Visited: {visitedCountries.length}</h3>
+            <h3>Total Flags Visited: {visitedFlags.length}</h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+                }
+            </ol>
+            <div className='visited-flags-container'>
+                {
+                    visitedFlags.map((flag, index) => <img key={index} src={flag}></img>)
+                }
+            </div>
+            <div className='countries'>
+                {
+                    countries.map(country => <Country 
+                        key={country.cca3.cca3} 
+                        country={country}
+                        handleVisitedCountries={handleVisitedCountries} handleVisitedFlags={handleVisitedFlags}></Country>)
+                }
+            </div>
         </div>
     );
 };
